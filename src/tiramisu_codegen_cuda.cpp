6ecb21ca30d95672647455d0d1623a9ccb2926fd
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-10-21 15:31:28 +0100

    change to std::cerr

--- src/tiramisu_codegen_cuda.cpp
+++ src/tiramisu_codegen_cuda.cpp
@@ -1594,15 +1594,15 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
             }
             return used_scalar;
         } else {
-            std::cout << "gpu_iterators contents: " << std::endl;
+            std::cerr << "gpu_iterators contents: " << std::endl;
             for (std::pair<std::string, cuda_ast::gpu_iterator> p : this->gpu_iterators)
                 std::cerr << p.first << ", ";
-            std::cout << std::endl;
+            std::cerr << std::endl;
 
-            std::cout << "m_scalar_data contents: " << std::endl;
+            std::cerr << "m_scalar_data contents: " << std::endl;
             for (std::pair<std::string, std::pair<tiramisu::primitive_t, cuda_ast::memory_location>> p : this->m_scalar_data)
                 std::cerr << p.first << ", ";
-            std::cout << std::endl;
+            std::cerr << std::endl;
             ERROR("Scalar not found: " + name, true);
         }
     }
