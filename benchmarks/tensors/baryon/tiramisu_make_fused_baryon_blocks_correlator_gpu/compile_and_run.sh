204a46617b3aa2a4d78065c76ea765c56dd5ffe5
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-10-21 14:18:59 +0100

    fix

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/compile_and_run.sh
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/compile_and_run.sh
@@ -1,1 +1,3 @@
-../../,,/compile_and_run_benchmarks.sh . tiramisu_make_fused_baryon_blocks_correlator
+cd ../../../
+./compile_and_run_benchmarks.sh ./tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/ tiramisu_make_fused_baryon_blocks_correlator
+cd -
