f27ca05f696db9a8120ecbf536b0d102803519e0
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-10-26 17:01:18 +0100

    try to fix size parameters

--- benchmarks/tensors/baryon/gpu/benchmarks.h
+++ benchmarks/tensors/baryon/gpu/benchmarks.h
@@ -14,47 +14,63 @@
 #define PRINT_OUTPUT 0 
 #define SIZE_IS_MULTIPLE_OF_TILE 1
 
-// Data size
-#if TIRAMISU_XLARGE
+// // Data size
+// #if TIRAMISU_XLARGE
 
-#define N 4096
-#define M N
-#define K N
-#define SIZE (65536*4096)
+// #define N 4096
+// #define M N
+// #define K N
+// #define SIZE (65536*4096)
 
-#elif TIRAMISU_LARGE
+// #elif TIRAMISU_LARGE
 
-#if SIZE_IS_MULTIPLE_OF_TILE
-#define N 1024
-#define M 1024
-#define K 1024
-#define SIZE (1024*1024)
-#else
-#define N 1060
-#define M 1060
-#define K 1060
-#endif
+// #if SIZE_IS_MULTIPLE_OF_TILE
+// #define N 1024
+// #define M 1024
+// #define K 1024
+// #define SIZE (1024*1024)
+// #else
+// #define N 1060
+// #define M 1060
+// #define K 1060
+// #endif
 
-#elif TIRAMISU_MEDIUM
+// #elif TIRAMISU_MEDIUM
 
-#define N 512
-#define M 512
-#define K 512
-#define SIZE (1024)
+// #define N 512
+// #define M 512
+// #define K 512
+// #define SIZE (1024)
 
-#elif TIRAMISU_SMALL
+// #elif TIRAMISU_SMALL
 
-#define N 128
-#define M 128
-#define K 128
-#define SIZE (128)
+// #define N 128
+// #define M 128
+// #define K 128
+// #define SIZE (128)
 
-#else
+// #else
 
-#define N 1024
-#define M 1024
-#define K 1024
+// #define N 1024
+// #define M 1024
+// #define K 1024
 
-#endif
+// #endif
+
+// BARYON_N is used for loop iterators.
+#define BARYON_N 16
+#define BX 16
+#define BY 16
+#define BZ 16
+#define BT 16
+#define BK 16
+
+// BARYON_P1 is used for the size of first dimension
+// of array and possible value of the parameters used
+// in that first dimension.
+#define BARYON_P1 3
+// BARYON_P is used for the size of the other array
+// dimensions that are not of size BARYON_N
+#define BARYON_P 1
 
 #endif /* BENCHMARKS_BENCHMARKS_H_ */
