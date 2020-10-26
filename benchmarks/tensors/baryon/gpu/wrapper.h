f00e302a317d3f65e6a125d1c52ea56733e7d7b2
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-10-26 17:08:06 +0100

    try to fix size parameters

--- benchmarks/tensors/baryon/gpu/wrapper.h
+++ benchmarks/tensors/baryon/gpu/wrapper.h
@@ -5,16 +5,16 @@
 // Define these values for each new test
 #define TEST_NAME_STR       "baryon"
 
-// Data size
-#if TIRAMISU_XLARGE
-#define SIZE (1024*1024*128)
-#elif TIRAMISU_LARGE
-#define SIZE (1024*1024)
-#elif TIRAMISU_MEDIUM
-#define SIZE (1024)
-#elif TIRAMISU_SMALL
-#define SIZE (128)
-#endif
+// // Data size
+// #if TIRAMISU_XLARGE
+// #define SIZE (1024*1024*128)
+// #elif TIRAMISU_LARGE
+// #define SIZE (1024*1024)
+// #elif TIRAMISU_MEDIUM
+// #define SIZE (1024)
+// #elif TIRAMISU_SMALL
+// #define SIZE (128)
+// #endif
 
 // --------------------------------------------------------
 // No need to modify anything in the following ------------
@@ -26,21 +26,21 @@
 extern "C" {
 #endif
 
-// BARYON_N is used for loop iterators.
-#define BARYON_N 16
-#define BX 16
-#define BY 16
-#define BZ 16
-#define BT 16
-#define BK 16
+// // BARYON_N is used for loop iterators.
+// #define BARYON_N 16
+// #define BX 16
+// #define BY 16
+// #define BZ 16
+// #define BT 16
+// #define BK 16
 
-// BARYON_P1 is used for the size of first dimension
-// of array and possible value of the parameters used
-// in that first dimension.
-#define BARYON_P1 3
-// BARYON_P is used for the size of the other array
-// dimensions that are not of size BARYON_N
-#define BARYON_P 1
+// // BARYON_P1 is used for the size of first dimension
+// // of array and possible value of the parameters used
+// // in that first dimension.
+// #define BARYON_P1 3
+// // BARYON_P is used for the size of the other array
+// // dimensions that are not of size BARYON_N
+// #define BARYON_P 1
 
 int tiramisu_generated_code(halide_buffer_t *,
 			    halide_buffer_t *,
