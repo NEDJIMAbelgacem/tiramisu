cefa5e4f0a9e43201a81b081984af7f90d54a9e4
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-10-30 02:03:13 +0100

    try fix

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -369,7 +369,7 @@ void generate_function(std::string name)
     // computation copy_snk_color_weights_host_to_device({}, memcpy(snk_color_weights_cpu, buf_snk_color_weights));
     // computation copy_snk_spin_weights_host_to_device({}, memcpy(snk_spin_weights_cpu, buf_snk_spin_weights));
     // computation copy_snk_weights_host_to_device({}, memcpy(snk_weights_cpu, buf_snk_weights));
-    computation copy_sigs_host_to_device({}, memcpy(sigs_cpu, buf_sigs));
+    // computation copy_sigs_host_to_device({}, memcpy(sigs_cpu, buf_sigs));
 
     computation copy_buf_C_r_device_to_host({}, memcpy(buf_C_r, buf_C_r_cpu));
     computation copy_buf_C_i_device_to_host({}, memcpy(buf_C_i, buf_C_i_cpu));
@@ -386,7 +386,7 @@ void generate_function(std::string name)
     // computation copy_snk_color_weights_device_to_host({}, memcpy(*snk_color_weights.get_buffer(), snk_color_weights_cpu));
     // computation copy_snk_spin_weights_device_to_host({}, memcpy(*snk_spin_weights.get_buffer(), snk_spin_weights_cpu));
     // computation copy_snk_weights_device_to_host({}, memcpy(*snk_weights.get_buffer(), snk_weights_cpu));
-    computation copy_sigs_device_to_host({}, memcpy(*sigs.get_buffer(), sigs_cpu));
+    // computation copy_sigs_device_to_host({}, memcpy(*sigs.get_buffer(), sigs_cpu));
 
     // -------------------------------------------------------
     // Layer III
@@ -445,7 +445,7 @@ void generate_function(std::string name)
                                 // .then(copy_snk_color_weights_host_to_device, computation::root)
                                 // .then(copy_snk_spin_weights_host_to_device, computation::root)
                                 // .then(copy_snk_weights_host_to_device, computation::root)
-                                .then(copy_sigs_host_to_device, computation::root)
+                                // .then(copy_sigs_host_to_device, computation::root)
                                 .then(C_init_r, computation::root);
 
     C_init_i.then(copy_buf_C_r_device_to_host, computation::root)
@@ -463,7 +463,7 @@ void generate_function(std::string name)
             // .then(copy_snk_color_weights_device_to_host, computation::root)
             // .then(copy_snk_spin_weights_device_to_host, computation::root)
             // .then(copy_snk_weights_device_to_host, computation::root)
-            .then(copy_sigs_device_to_host, computation::root);
+            // .then(copy_sigs_device_to_host, computation::root);
 
 
 #if VECTORIZED
