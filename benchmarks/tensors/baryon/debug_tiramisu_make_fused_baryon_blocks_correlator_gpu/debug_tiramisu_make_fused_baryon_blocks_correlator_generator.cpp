5a631aecb15147923bc03bc82bfd9088d50aa981
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-11-02 09:24:13 +0100

    change

--- benchmarks/tensors/baryon/debug_tiramisu_make_fused_baryon_blocks_correlator_gpu/debug_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/debug_tiramisu_make_fused_baryon_blocks_correlator_gpu/debug_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -65,8 +65,8 @@ void generate_function(std::string name)
 
     buf_C_r.tag_gpu_global();
     buf_C_i.tag_gpu_global();
-    // buffer buf_C_r_cpu("buf_C_r_cpu", {t, x_out, rp, m, r, n}, p_float64, a_temporary);
-    // buffer buf_C_i_cpu("buf_C_i_cpu", {t, x_out, rp, m, r, n}, p_float64, a_temporary);
+    buffer buf_C_r_cpu("buf_C_r_cpu", {t, x_out, rp, m, r, n}, p_float64, a_temporary);
+    buffer buf_C_i_cpu("buf_C_i_cpu", {t, x_out, rp, m, r, n}, p_float64, a_temporary);
     // buffer B1_prop_r_cpu("B1_prop_r_cpu",   {t, iCprime, iSprime, jCprime, jSprime, x, y, tri}, p_float64, a_temporary);
     // buffer B1_prop_i_cpu("B1_prop_i_cpu",   {t, iCprime, iSprime, jCprime, jSprime, x, y, tri}, p_float64, a_temporary);
     // buffer src_psi_B1_r_cpu("src_psi_B1_r_cpu",    {y, m}, p_float64, a_temporary);
