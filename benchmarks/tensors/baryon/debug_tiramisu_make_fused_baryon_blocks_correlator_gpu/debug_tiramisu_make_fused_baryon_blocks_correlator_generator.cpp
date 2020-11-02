bb546ae2914d819f79d88035b5ceada2346f5e4a
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-11-02 09:14:28 +0100

    change

--- benchmarks/tensors/baryon/debug_tiramisu_make_fused_baryon_blocks_correlator_gpu/debug_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/debug_tiramisu_make_fused_baryon_blocks_correlator_gpu/debug_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -453,7 +453,7 @@ void generate_function(std::string name)
     //         .then(copy_sigs_host_to_device, computation::root)
             // .then(C_init_r, computation::root)
             // .then(C_init_i, computation::root)
-            );
+            // );
 
     // handle = &handle->then(C_init_i, computation::root);
 
