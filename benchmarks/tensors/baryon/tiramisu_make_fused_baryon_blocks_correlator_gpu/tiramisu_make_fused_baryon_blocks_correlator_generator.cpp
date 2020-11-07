45d1edd8bbb91457663057249fd9869867308ab7
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-11-08 00:24:27 +0100

    change

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -273,22 +273,22 @@ void generate_function(std::string name)
         // &sigs_cpu
         // }, 
 
-    buffer buf_C_r_cpu("buf_C_r_cpu", {Lt, Vsnk/sites_per_rank, B1Nrows, NsrcHex, B1Nrows, NsnkHex}, p_float64, a_input);
-    buffer buf_C_i_cpu("buf_C_i_cpu", {Lt, Vsnk/sites_per_rank, B1Nrows, NsrcHex, B1Nrows, NsnkHex}, p_float64, a_input);
-    buffer B1_prop_r_cpu("B1_prop_r_cpu",   {Lt, Nc, Ns, Nc, Ns, Vsnk, Vsrc, Nq}, p_float64, a_input);
-    buffer B1_prop_i_cpu("B1_prop_i_cpu",   {Lt, Nc, Ns, Nc, Ns, Vsnk, Vsrc, Nq}, p_float64, a_input);
-    buffer src_psi_B1_r_cpu("src_psi_B1_r_cpu",    {Vsrc, NsrcHex}, p_float64, a_input);
-    buffer src_psi_B1_i_cpu("src_psi_B1_i_cpu",    {Vsrc, NsrcHex}, p_float64, a_input);
-    buffer snk_psi_r_cpu("snk_psi_r_cpu", {Vsnk, NsnkHex}, p_float64, a_input);
-    buffer snk_psi_i_cpu("snk_psi_i_cpu", {Vsnk, NsnkHex}, p_float64, a_input);
-    buffer src_color_weights_cpu("src_color_weights_cpu", {B1Nrows, Nw, Nq}, p_int32, a_input);
-    buffer src_spin_weights_cpu("src_spin_weights_cpu", {B1Nrows, Nw, Nq}, p_int32, a_input);
-    buffer src_weights_cpu("src_weights_cpu", {B1Nrows, Nw}, p_float64, a_input);
-    buffer snk_color_weights_cpu("snk_color_weights_cpu", {B1Nrows, B1Nperms, Nw, Nq}, p_int32, a_input);
-    buffer snk_spin_weights_cpu("snk_spin_weights_cpu", {B1Nrows, B1Nperms, Nw, Nq}, p_int32, a_input);
-    buffer snk_weights_cpu("snk_weights_cpu", {B1Nrows, Nw}, p_float64, a_input);
-    buffer src_spins_cpu("src_spins_cpu", {B1Nrows}, p_int32, a_input);
-    buffer sigs_cpu("sigs_cpu", {B1Nperms}, p_int32, a_input);
+    buffer buf_C_r_cpu("buf_C_r_cpu", {Lt, Vsnk/sites_per_rank, B1Nrows, NsrcHex, B1Nrows, NsnkHex}, p_float64, a_temporary);
+    buffer buf_C_i_cpu("buf_C_i_cpu", {Lt, Vsnk/sites_per_rank, B1Nrows, NsrcHex, B1Nrows, NsnkHex}, p_float64, a_temporary);
+    buffer B1_prop_r_cpu("B1_prop_r_cpu",   {Lt, Nc, Ns, Nc, Ns, Vsnk, Vsrc, Nq}, p_float64, a_temporary);
+    buffer B1_prop_i_cpu("B1_prop_i_cpu",   {Lt, Nc, Ns, Nc, Ns, Vsnk, Vsrc, Nq}, p_float64, a_temporary);
+    buffer src_psi_B1_r_cpu("src_psi_B1_r_cpu",    {Vsrc, NsrcHex}, p_float64, a_temporary);
+    buffer src_psi_B1_i_cpu("src_psi_B1_i_cpu",    {Vsrc, NsrcHex}, p_float64, a_temporary);
+    buffer snk_psi_r_cpu("snk_psi_r_cpu", {Vsnk, NsnkHex}, p_float64, a_temporary);
+    buffer snk_psi_i_cpu("snk_psi_i_cpu", {Vsnk, NsnkHex}, p_float64, a_temporary);
+    buffer src_color_weights_cpu("src_color_weights_cpu", {B1Nrows, Nw, Nq}, p_int32, a_temporary);
+    buffer src_spin_weights_cpu("src_spin_weights_cpu", {B1Nrows, Nw, Nq}, p_int32, a_temporary);
+    buffer src_weights_cpu("src_weights_cpu", {B1Nrows, Nw}, p_float64, a_temporary);
+    buffer snk_color_weights_cpu("snk_color_weights_cpu", {B1Nrows, B1Nperms, Nw, Nq}, p_int32, a_temporary);
+    buffer snk_spin_weights_cpu("snk_spin_weights_cpu", {B1Nrows, B1Nperms, Nw, Nq}, p_int32, a_temporary);
+    buffer snk_weights_cpu("snk_weights_cpu", {B1Nrows, Nw}, p_float64, a_temporary);
+    buffer src_spins_cpu("src_spins_cpu", {B1Nrows}, p_int32, a_temporary);
+    buffer sigs_cpu("sigs_cpu", {B1Nperms}, p_int32, a_temporary);
 
     buffer buf_B1_Blocal_r1_r("buf_B1_Blocal_r1_r",   {Nc, Ns, Nc, Ns, Nc, Ns, NsrcHex}, p_float64, a_temporary);
     buffer buf_B1_Blocal_r1_i("buf_B1_Blocal_r1_i",   {Nc, Ns, Nc, Ns, Nc, Ns, NsrcHex}, p_float64, a_temporary);
