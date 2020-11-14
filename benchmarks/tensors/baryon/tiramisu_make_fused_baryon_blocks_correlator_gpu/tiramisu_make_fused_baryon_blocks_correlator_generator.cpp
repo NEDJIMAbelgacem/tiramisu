cfdec503b4a3ff4a0571555a176aad82247d67cb
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-11-14 01:51:43 +0100

    change

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -591,7 +591,7 @@ void generate_function(std::string name)
     computation* handle = &C_init_r.then(C_init_i, computation::root);
 
     // computation* handle = &copy_buf_C_r_host_to_device.then(copy_buf_C_i_host_to_device, computation::root);
-    handle = &(handle->then(copy_buf_C_r_host_to_device, computation::root).then(copy_buf_C_i_host_to_device, computation::root))
+    handle = &(handle->then(copy_buf_C_r_host_to_device, computation::root).then(copy_buf_C_i_host_to_device, computation::root));
 
     handle = &(handle->
              then(copy_B1_prop_r_host_to_device, computation::root)
