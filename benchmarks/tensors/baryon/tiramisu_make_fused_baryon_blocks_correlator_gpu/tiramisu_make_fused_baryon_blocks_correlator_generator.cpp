aa06d6756b5890c40e6e2c2260a9fa8ab803b130
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-11-30 06:34:46 +0100

    change

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -460,31 +460,8 @@ void generate_function(std::string name)
 
     // handle = &handle->then(C_init_i, computation::root);
 
-    handle = &(handle
-        ->then(B1_Blocal_r1_r_init, x_in)
-        .then(B1_Blocal_r1_i_init, jSprime)
-        .then(B1_Blocal_r1_r_props_init, x_in)
-        .then(B1_Blocal_r1_i_props_init, jSprime)
-        .then(B1_Blocal_r1_r_diquark, y)
-        .then(B1_Blocal_r1_i_diquark, wnumBlock)
-        .then(B1_Blocal_r1_r_props, wnumBlock)
-        .then(B1_Blocal_r1_i_props, jSprime)
-        .then(B1_Blocal_r1_r_update, y)
-        .then(B1_Blocal_r1_i_update, m)
-        .then(B1_Blocal_r2_r_init, x_in)
-        .then(B1_Blocal_r2_i_init, jSprime)
-        .then(B1_Blocal_r2_r_props_init, x_in)
-        .then(B1_Blocal_r2_i_props_init, jSprime)
-        .then(B1_Blocal_r2_r_diquark, y)
-        .then(B1_Blocal_r2_i_diquark, wnumBlock)
-        .then(B1_Blocal_r2_r_props, wnumBlock)
-        .then(B1_Blocal_r2_i_props, jSprime)
-        .then(B1_Blocal_r2_r_update, y)
-        .then(B1_Blocal_r2_i_update, m));
-
-    // first the x only arrays
     // handle = &(handle
-    //     ->then(B1_Blocal_r1_r_init, t2)
+    //     ->then(B1_Blocal_r1_r_init, x_in)
     //     .then(B1_Blocal_r1_i_init, jSprime)
     //     .then(B1_Blocal_r1_r_props_init, x_in)
     //     .then(B1_Blocal_r1_i_props_init, jSprime)
@@ -504,6 +481,29 @@ void generate_function(std::string name)
     //     .then(B1_Blocal_r2_i_props, jSprime)
     //     .then(B1_Blocal_r2_r_update, y)
     //     .then(B1_Blocal_r2_i_update, m));
+
+    // first the x only arrays
+    handle = &(handle
+        ->then(B1_Blocal_r1_r_init, t2)
+        .then(B1_Blocal_r1_i_init, jSprime)
+        .then(B1_Blocal_r1_r_props_init, x_in)
+        .then(B1_Blocal_r1_i_props_init, jSprime)
+        .then(B1_Blocal_r1_r_diquark, y)
+        .then(B1_Blocal_r1_i_diquark, wnumBlock)
+        .then(B1_Blocal_r1_r_props, wnumBlock)
+        .then(B1_Blocal_r1_i_props, jSprime)
+        .then(B1_Blocal_r1_r_update, y)
+        .then(B1_Blocal_r1_i_update, m)
+        .then(B1_Blocal_r2_r_init, x_in)
+        .then(B1_Blocal_r2_i_init, jSprime)
+        .then(B1_Blocal_r2_r_props_init, x_in)
+        .then(B1_Blocal_r2_i_props_init, jSprime)
+        .then(B1_Blocal_r2_r_diquark, y)
+        .then(B1_Blocal_r2_i_diquark, wnumBlock)
+        .then(B1_Blocal_r2_r_props, wnumBlock)
+        .then(B1_Blocal_r2_i_props, jSprime)
+        .then(B1_Blocal_r2_r_update, y)
+        .then(B1_Blocal_r2_i_update, m));
 
     handle = &(handle 
           ->then(C_prop_init_r, x_in) 
