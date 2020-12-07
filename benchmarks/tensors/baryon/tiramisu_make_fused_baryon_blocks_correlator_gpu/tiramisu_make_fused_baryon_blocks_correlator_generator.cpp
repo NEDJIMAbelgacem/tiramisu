d85ce35795c8b1a14bceaf12ccabece20e640a66
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-12-07 15:23:30 +0100

    change

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -463,7 +463,7 @@ void generate_function(std::string name)
         ->then(B1_Blocal_r1_r_init, computation::root)
         .then(B1_Blocal_r1_i_init, jSprime)
         //
-        .then(B1_Blocal_r1_r_props_init, x_in)
+        .then(B1_Blocal_r1_r_props_init, t)
         .then(B1_Blocal_r1_i_props_init, jSprime)
         .then(B1_Blocal_r1_r_diquark, y)
         .then(B1_Blocal_r1_i_diquark, wnumBlock)
