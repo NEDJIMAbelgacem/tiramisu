4c06a7d80e799162763c8a796032b6a53df83fd6
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-12-10 12:15:48 +0100

    change

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_ref.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_ref.cpp
@@ -69,8 +69,8 @@ void make_local_block(double* Blocal_re,
    for (wnum=0; wnum<Nw_f; wnum++) {
       iC = color_weights[index_2d(wnum,0, Nq_f)];
       iS = spin_weights[index_2d(wnum,0, Nq_f)];
-      jC = color_weights[index_2d(wnum,1, Nq_f)];
-      jS = spin_weights[index_2d(wnum,1, Nq_f)];
+      jC = color_weights[index_2d(wnum,1, 0)];
+      jS = spin_weights[index_2d(wnum,1, 0)];
       kC = color_weights[index_2d(wnum,2, Nq_f)];
       kS = spin_weights[index_2d(wnum,2, Nq_f)];
       for (iCprime=0; iCprime<Nc_f; iCprime++) {
@@ -84,6 +84,7 @@ void make_local_block(double* Blocal_re,
                      for (jCprime=0; jCprime<Nc_f; jCprime++) {
                         for (jSprime=0; jSprime<Ns_f; jSprime++) {
                            std::complex<double> prop_1(prop_re[prop_index(1,t,jC,jS,jCprime,jSprime,y,x ,Nc_f,Ns_f,Vsrc_f,Vsnk_f,Nt_f)], prop_im[prop_index(1,t,jC,jS,jCprime,jSprime,y,x ,Nc_f,Ns_f,Vsrc_f,Vsnk_f,Nt_f)]);
+                           // std::complex<double> prop_1(jC, jS);
                            prop_prod = /*prop_prod_02 **/ prop_1;
                            for (m=0; m<Nsrc_f; m++) {
                               std::complex<double> psi(psi_re[index_2d(y,m ,Nsrc_f)], psi_im[index_2d(y,m ,Nsrc_f)]);
