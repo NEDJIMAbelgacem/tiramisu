79f95e90b3472006a54527af1b709a9795c96ed0
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-12-13 20:28:43 +0100

    change

--- benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/tiramisu_make_fused_baryon_blocks_correlator_gpu/tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -381,29 +381,27 @@ void generate_function(std::string name)
 // {t, x_out, x_in, rp, m, r, n}
     C_init_r.tag_gpu_level(x_out, x_in);
     C_init_i.tag_gpu_level(x_out, x_in);
-
 // {t, x_out, x_in, iCprime, iSprime, kCprime, kSprime, jCprime, jSprime, m}
     B1_Blocal_r1_r_init.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
     B1_Blocal_r1_i_init.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
 
 // {t, x_out, x_in, iCprime, iSprime, kCprime, kSprime, y, jCprime, jSprime}
-    B1_Blocal_r1_r_props_init.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
-    B1_Blocal_r1_i_props_init.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_r_props_init.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_i_props_init.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
 // {t, x_out, x_in, iCprime, iSprime, kCprime, kSprime, y, wnumBlock}
-    B1_Blocal_r1_r_diquark.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
-    B1_Blocal_r1_i_diquark.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_r_diquark.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_i_diquark.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
 // {t, x_out, x_in, iCprime, iSprime, kCprime, kSprime, y, jCprime, jSprime}
-    B1_Blocal_r1_r_props.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
-    B1_Blocal_r1_i_props.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_r_props.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_i_props.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
 // {t, x_out, x_in, iCprime, iSprime, kCprime, kSprime, y, jCprime, jSprime, m}
-    B1_Blocal_r1_r_update.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
-    B1_Blocal_r1_i_update.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_r_update.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
+    B1_Blocal_r1_i_update.tag_gpu_level(x_out, x_in);//, iCprime, iSprime, kCprime, kSprime);
 
 
 // {t, x_out, x_in, iCprime, iSprime, kCprime, kSprime, jCprime, jSprime, m}
     B1_Blocal_r2_r_init.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
     B1_Blocal_r2_i_init.tag_gpu_level(x_out, x_in, iCprime, iSprime, kCprime, kSprime);
-
 
     B1_Blocal_r2_r_props_init.tag_gpu_level(x_out, x_in);
     B1_Blocal_r2_i_props_init.tag_gpu_level(x_out, x_in);
