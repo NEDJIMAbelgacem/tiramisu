4e8482561ed7705d4601c8e196ce4aff0d243004
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2020-12-27 19:33:58 -0500

    Add generated files

---
+++ build/benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o"
+  "generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o_cpu.o"
+  "generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o_gpu.o"
+  "CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.dir/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.cpp.o"
+  "../../../../gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.pdb"
+  "../../../../gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/CMakeFiles/run_gpu_tiramisu_make_fused_baryon_blocks_correlator_correctness.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_gpu_tiramisu_make_fused_baryon_blocks_correlator_correctness"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run_gpu_tiramisu_make_fused_baryon_blocks_correlator_correctness.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
