1cde3403b98195defeac6452dd8b43bc09ec7749
Belgacem <gb_nedjima@esi.dz>
2020-12-28 03:12:25 +0100

    change to splitted

--- benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
@@ -467,10 +467,10 @@ void generate_function(std::string name)
 
     // // first the x only arrays
     handle = &(handle
-        ->then(B1_Blocal_r1_r_init, x_in)
+        ->then(B1_Blocal_r1_r_init, computation::root)
         .then(B1_Blocal_r1_i_init, jSprime)
         //
-        .then(B1_Blocal_r1_r_props_init, x_in)
+        .then(B1_Blocal_r1_r_props_init, computation::root)
         .then(B1_Blocal_r1_i_props_init, jSprime)
         .then(B1_Blocal_r1_r_diquark, y)
         .then(B1_Blocal_r1_i_diquark, wnumBlock)
@@ -479,10 +479,10 @@ void generate_function(std::string name)
         .then(B1_Blocal_r1_r_update, y)
         .then(B1_Blocal_r1_i_update, m)
         //
-        .then(B1_Blocal_r2_r_init, x_in)
+        .then(B1_Blocal_r2_r_init, computation::root)
         .then(B1_Blocal_r2_i_init, jSprime)
         //
-        .then(B1_Blocal_r2_r_props_init, x_in)
+        .then(B1_Blocal_r2_r_props_init, computation::root)
         .then(B1_Blocal_r2_i_props_init, jSprime)
         .then(B1_Blocal_r2_r_diquark, y)
         .then(B1_Blocal_r2_i_diquark, wnumBlock)
@@ -494,7 +494,7 @@ void generate_function(std::string name)
 
 
     handle = &(handle 
-          ->then(C_prop_init_r, x_in) 
+          ->then(C_prop_init_r, computation::root) 
           .then(C_prop_init_i, r)
           .then( *(new_term_0_r1_b1.get_real()), r)
           .then( *(new_term_0_r1_b1.get_imag()), wnum)
