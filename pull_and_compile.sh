a8819f3b9ffade0ae4228b15c76580c5fe61744a
Belgacem <gb_nedjima@esi.dz>
2021-01-04 02:17:06 +0100

    change

---
+++ pull_and_compile.sh
@@ -0,0 +1,6 @@
+git pull belgacem_tiramisu dibaryon-gpu
+cd ./build
+make run_gpu_tiramisu_make_fused_dibaryon_blocks_correlator &> out.txt
+mv ./out.txt ../out.txt
+git add *
+git commit -m "change"
