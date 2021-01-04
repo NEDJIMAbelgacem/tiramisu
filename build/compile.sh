65db6a790944ec057b724199a1722148e7832ebe
Belgacem <gb_nedjima@esi.dz>
2021-01-04 14:04:47 +0100

    change

--- build/compile.sh
+++ build/compile.sh
@@ -1,4 +1,8 @@
 cd /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build/benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator
+
+echo "==============================================================="
+echo "COMPILE GENERATOR"
+echo "==============================================================="
 # compile generator
 g++   -DNVCC_PATH=\"/usr/local/cuda/bin/nvcc\" \
 -I/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/include \
@@ -11,6 +15,10 @@ g++   -DNVCC_PATH=\"/usr/local/cuda/bin/nvcc\" \
 -O0   -o CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.dir/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp.o \
 -c /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.cpp
 
+echo "==============================================================="
+echo "LINK GENERATOR"
+echo "==============================================================="
+
 # link generator
 /usr/bin/cmake -E cmake_link_script CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.dir/link.txt --verbose=1
 g++   -std=c++11 -Wall -Wno-sign-compare -fno-rtti -DHALIDE_NO_JPEG -g -O0  \
@@ -20,10 +28,18 @@ CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_generator.dir/gpu_ti
 ../../../../libcuda_wrapper.a /usr/local/cuda/lib64/libcudart_static.a -lpthread -ldl /usr/lib/x86_64-linux-gnu/librt.so \
 /usr/local/cuda/lib64/libcublas.so -Wl,-rpath,/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build:/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/3rdParty/isl/build/lib:/usr/local/cuda/lib64 
 
+echo "==============================================================="
+echo "RUN GENERATOR"
+echo "==============================================================="
+
 # run generator
 # ../../../../gpu_tiramisu_make_fused_baryon_blocks_correlator_generator
 /usr/local/cuda/bin/nvcc --default-stream per-thread -x cu -c -dc generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o.cu.edited -o generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o_cpu.o
 /usr/local/cuda/bin/nvcc --default-stream per-thread -dlink generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o_cpu.o -o generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o_gpu.o
+
+echo "==============================================================="
+echo "COMPILE WRAPPER"
+echo "==============================================================="
 # compile wrapper
 /usr/bin/cmake -E cmake_link_script CMakeFiles/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.dir/link.txt --verbose=1
 g++   -std=c++11 -Wall -Wno-sign-compare -fno-rtti -DHALIDE_NO_JPEG -g -O0  \
@@ -33,5 +49,9 @@ generated_gpu_tiramisu_make_fused_baryon_blocks_correlator.o_gpu.o  -o ../../../
 ../../../../libtiramisu.so ../../../../../3rdParty/Halide/lib/libHalide.a ../../../../../3rdParty/isl/build/lib/libisl.so -ldl -lpthread -lrt -ldl \
 -lpthread -lz -lm ../../../../libcuda_wrapper.a /usr/local/cuda/lib64/libcudart_static.a -lpthread -ldl /usr/lib/x86_64-linux-gnu/librt.so \
 /usr/local/cuda/lib64/libcublas.so -Wl,-rpath,/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build:/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/3rdParty/isl/build/lib:/usr/local/cuda/lib64
+
+echo "==============================================================="
+echo "RUN WRAPPER"
+echo "==============================================================="
 # run wrapper
 ../../../../gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper
