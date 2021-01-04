fca38beed73f63de6e9c5be869531e70059a2329
Belgacem <gb_nedjima@esi.dz>
2021-01-04 13:47:06 +0100

    change size

--- benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.cpp
+++ benchmarks/tensors/baryon/gpu_tiramisu_make_fused_baryon_blocks_correlator/gpu_tiramisu_make_fused_baryon_blocks_correlator_wrapper.cpp
@@ -464,10 +464,10 @@ int main(int, char **)
    std::cout << "End reference C code." <<  std::endl;
 #endif
 
-   //  print_time("performance_CPU.csv", "dibaryon", {"Tiramisu"}, {median(duration_vector_1)/1000.});
+    print_time("performance_CPU.csv", "dibaryon", {"Tiramisu"}, {median(duration_vector_1)/1000.});
 
 #if RUN_CHECK
-    print_time("performance_CPU.csv", std::string( "dibaryon_" ) + std::to_string(P_size), {"Ref", "Tiramisu"}, {median(duration_vector_2)/1000., median(duration_vector_1)/1000.});
+   //  print_time("performance_CPU.csv", std::string( "dibaryon_" ) + std::to_string(P_size), {"Ref", "Tiramisu"}, {median(duration_vector_2)/1000., median(duration_vector_1)/1000.});
     std::cout << "\nSpeedup = " << median(duration_vector_2)/median(duration_vector_1) << std::endl;
     
    for (rp=0; rp<B1Nrows; rp++) {
