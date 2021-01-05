06c976183d3f7ecd0aff45a6454fe22f57055903
Belgacem <gb_nedjima@esi.dz>
2021-01-05 17:26:04 +0100

    change

--- src/tiramisu_core.cpp
+++ src/tiramisu_core.cpp
@@ -5586,7 +5586,7 @@ tiramisu::buffer::buffer(std::string name, std::vector<tiramisu::expr> dim_sizes
     assert(fct != NULL && "Input function is NULL");
 
     // Check that the buffer does not already exist.
-    if ((fct->get_buffers().count(name) == 0) && ("Buffer already exists"))
+    if (fct->get_buffers().count(name) != 0)
     {
         std::cout << "Error while looking for: " << name << std::endl;
     }
