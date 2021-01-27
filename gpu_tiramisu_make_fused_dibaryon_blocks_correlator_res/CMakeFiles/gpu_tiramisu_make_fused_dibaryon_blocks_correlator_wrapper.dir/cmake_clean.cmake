efbcd637e7f7fbb0b044caa005ecb34b2ce89731
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-01-27 04:16:09 -0500

    upload results

---
+++ gpu_tiramisu_make_fused_dibaryon_blocks_correlator_res/CMakeFiles/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o"
+  "generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_cpu.o"
+  "generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_gpu.o"
+  "CMakeFiles/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.dir/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.cpp.o"
+  "../../../../../build/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.pdb"
+  "../../../../../build/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gpu_tiramisu_make_fused_dibaryon_blocks_correlator_res/CMakeFiles/run_gpu_tiramisu_make_fused_dibaryon_blocks_correlator_nvprof.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_gpu_tiramisu_make_fused_dibaryon_blocks_correlator_nvprof"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run_gpu_tiramisu_make_fused_dibaryon_blocks_correlator_nvprof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
