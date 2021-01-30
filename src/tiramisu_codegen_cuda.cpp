0fe9b306721f547ca6a4fa4a40c5350fa64301eb
Belgacem <gb_nedjima@esi.dz>
2021-01-30 14:16:17 +0100

    fix compilation

--- src/tiramisu_codegen_cuda.cpp
+++ src/tiramisu_codegen_cuda.cpp
@@ -1836,7 +1836,7 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
 
         stringstream command;
         command << NVCC_PATH;
-        command << " -g"
+        command << " -g -G";
         // Basic streaming for parallelization
         command << " --default-stream per-thread";
         // Say that this is actually cuda code
@@ -1870,7 +1870,7 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
 
         stringstream command;
         command << NVCC_PATH;
-        command << " -g -G";
+        command << " -g";
         // Basic streaming for parallelization
         command << " --default-stream per-thread";
         // Link device object code
