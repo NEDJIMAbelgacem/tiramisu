ccf9ddd4c8a0923497ced772a9620eb3aa98509b
dell <dell@DESKTOP-9F3L8S5>
2021-02-11 02:44:04 +0100

    add simple expression for sum_reduce

--- src/tiramisu_expr.cpp
+++ src/tiramisu_expr.cpp
@@ -600,4 +600,15 @@ expr flexnlp_finalize()
             tiramisu::p_int32);
 }
 
+expr cub_sum_reduce(const buffer &A, const buffer &B, int nA)
+{
+    std::string fname = "tiramisu_reduce_add";
+    return expr(o_call, fname,
+            {
+                var(p_void_ptr, A.get_name()),
+                var(p_void_ptr, B.get_name()),
+                nA
+            },
+            tiramisu::p_uint8);
 }
+
