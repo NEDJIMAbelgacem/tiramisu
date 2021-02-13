c6778ccd22317c8a16e917cd27923ffe5317ef6c
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-02-13 16:58:04 +0100

    fix compilation

--- tutorials/developers/tutorial_reduction/tutorial_reduction.cpp
+++ tutorials/developers/tutorial_reduction/tutorial_reduction.cpp
@@ -10,14 +10,10 @@ int main(int argc, char **argv)
     var b_i("b_i", 0, 1);
     input A("A", { a_i }, p_float64);
     input B("B", { b_i }, p_float64);
-    buffer buf_A("buf_A",   { a_i }, p_float64, a_temporary);
-    buffer buf_B("buf_B",   { b_i }, p_float64, a_temporary);
-    A.store_in( &buf_A );
-    B.store_in( &buf_B );
 
     computation reduce({var("dummy", 0, 1)}, cub_sum_reduce(*A.get_buffer(), *B.get_buffer(), expr( 100 ) ));
 
-    tiramisu::codegen({&buf_A, &buf_B},
+    tiramisu::codegen({A.get_buffer(), B.get_buffer()},
                       "build/generated_fct_developers_tutorial_reduction.o");
 
     return 0;
