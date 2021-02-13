29b6dfd62929a4287f4b4b20fc78679c07b86508
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-02-13 18:40:30 +0100

    use seconds for measurement

--- tutorials/developers/tutorial_reduction/wrapper_tutorial_reduction.cpp
+++ tutorials/developers/tutorial_reduction/wrapper_tutorial_reduction.cpp
@@ -18,13 +18,15 @@ void test_reduction(const std::string &name, int array_size)
         auto start = std::chrono::steady_clock::now();
         for (int i = 0; i < array_size; ++i) sum += A(i);
         auto end = std::chrono::steady_clock::now();
-        seq_time = (end - start).count();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        seq_time = elapsed_seconds.count();
     }
     {
         auto start = std::chrono::steady_clock::now();
         tutorial_reduction( A.raw_buffer(), B.raw_buffer() );
         auto end = std::chrono::steady_clock::now();
-        reduce_time = (end - start).count();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        reduce_time = elapsed_seconds.count();
     }
     
     std::cout << "Sequential sum: " << sum << ", done in " << seq_time << "\n";
