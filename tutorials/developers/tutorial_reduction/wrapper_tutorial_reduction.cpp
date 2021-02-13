5dc0a5dfef258e67977cea63649995f987780050
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-02-13 18:30:42 +0100

    fix compilation

--- tutorials/developers/tutorial_reduction/wrapper_tutorial_reduction.cpp
+++ tutorials/developers/tutorial_reduction/wrapper_tutorial_reduction.cpp
@@ -18,13 +18,13 @@ void test_reduction(const std::string &name)
         auto start = std::chrono::steady_clock::now();
         for (int i = 0; i < 100; ++i) sum += A(i);
         auto end = std::chrono::steady_clock::now();
-        seq_time = (end - start).count()
+        seq_time = (end - start).count();
     }
     {
         auto start = std::chrono::steady_clock::now();
         tutorial_reduction( A.raw_buffer(), B.raw_buffer() );
         auto end = std::chrono::steady_clock::now();
-        reduce_time = (end - start).count()
+        reduce_time = (end - start).count();
     }
     
     std::cout << "Sequential sum: " << sum << ", done in " << seq_time << "\n";
