d6c7dc710f1e120076ded6c22bea00ba06ca6062
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-02-13 16:56:23 +0100

    fix compilation

--- tutorials/developers/tutorial_reduction/tutorial_reduction.cpp
+++ tutorials/developers/tutorial_reduction/tutorial_reduction.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 
     computation reduce({var("dummy", 0, 1)}, cub_sum_reduce(*A.get_buffer(), *B.get_buffer(), expr( 100 ) ));
 
-    tiramisu::codegen({A.get_buffer(), B.get_buffer()},
+    tiramisu::codegen({buf_A, buf_B},
                       "build/generated_fct_developers_tutorial_reduction.o", true);
 
     return 0;
