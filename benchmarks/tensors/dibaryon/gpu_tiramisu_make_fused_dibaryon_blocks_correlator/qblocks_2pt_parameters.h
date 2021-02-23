86b3744965bb887b16f4c953cb79abb631f716b3
NEDJIMAbelgacem <gb_nedjima@esi,dz>
2021-02-23 02:31:31 +0100

    replace sizes

--- benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/qblocks_2pt_parameters.h
+++ benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/qblocks_2pt_parameters.h
@@ -6,41 +6,13 @@
 
 #if DATA_SET_1
 
-// #define P_Vsrc 512
-// #define P_Vsnk 512
-// #define P_Nsrc 44
-// #define P_Nsnk 44
-// #define P_NEntangled 3
-// #define P_NsrcHex 1
-// #define P_NsnkHex 1
-// #define P_Nperms 36
-// #define P_B1Nperms 2
-// #define P_Nw 12
-// #define P_Nw2 288
-// #define P_Nw2Hex 32
-// #define P_Nt 2/*1*/
-// #define P_Nc 3
-// #define P_Ns 2
-// #define P_Nq 3
-// #define P_B2Nrows 4
-// #define P_B1Nrows 2
-// #define P_Nb 2
-// #define P_mq 1.0
-// #define P_Mq 2
-// #define P_B0Nrows 1
-// #define P_Mw 12
-// #define P_NsFull 4
-// #define P_sites_per_rank 128
-// #define P_src_sites_per_rank 4
-
-#define P_size 512
-#define P_Vsrc P_size
-#define P_Vsnk P_size
-#define P_Nsrc 2
-#define P_Nsnk 2
+#define P_Vsrc 512
+#define P_Vsnk 512
+#define P_Nsrc 44
+#define P_Nsnk 44
 #define P_NEntangled 3
-#define P_NsrcHex 2
-#define P_NsnkHex 2
+#define P_NsrcHex 1
+#define P_NsnkHex 1
 #define P_Nperms 36
 #define P_B1Nperms 2
 #define P_Nw 12
@@ -55,43 +27,16 @@
 #define P_Nb 2
 #define P_mq 1.0
 #define P_Mq 2
-#define P_B0Nrows 2
+#define P_B0Nrows 1
 #define P_Mw 12
 #define P_NsFull 4
 #define P_sites_per_rank 64 // threads per block
-#define P_src_sites_per_rank 8 // blocks count
+#define P_src_sites_per_rank 8 // blocks
 
 #elif DATA_SET_2
 
-// #define P_Vsrc 8
-// #define P_Vsnk 8
-// #define P_Nsrc 44
-// #define P_Nsnk 44
-// #define P_NEntangled 3
-// #define P_NsrcHex 1
-// #define P_NsnkHex 1
-// #define P_Nperms 36
-// #define P_B1Nperms 2
-// #define P_Nw 12
-// #define P_Nw2 288
-// #define P_Nw2Hex 32
-// #define P_Nt 1
-// #define P_Nc 3
-// #define P_Ns 2
-// #define P_Nq 3
-// #define P_B2Nrows 4
-// #define P_B1Nrows 2
-// #define P_Nb 2
-// #define P_mq 1.0
-// #define P_Mq 2
-// #define P_B0Nrows 1
-// #define P_Mw 12
-// #define P_NsFull 4
-// #define P_sites_per_rank 2
-// #define P_src_sites_per_rank 4
-
-#define P_Vsrc 16
-#define P_Vsnk 16
+#define P_Vsrc 8
+#define P_Vsnk 8
 #define P_Nsrc 44
 #define P_Nsnk 44
 #define P_NEntangled 3
@@ -114,8 +59,8 @@
 #define P_B0Nrows 1
 #define P_Mw 12
 #define P_NsFull 4
-#define P_sites_per_rank 8
-#define P_src_sites_per_rank 2 
+#define P_sites_per_rank 4
+#define P_src_sites_per_rank 4
 
 #endif
 
