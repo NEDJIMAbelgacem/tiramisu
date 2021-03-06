f2719205e471d8689520f1bfe2cf7978f9fed4a5
NEDJIMAbelgacem <gb_nedjima@esi,dz>
2021-03-06 16:22:43 +0100

    add quote to file path

--- src/tiramisu_codegen_cuda.cpp
+++ src/tiramisu_codegen_cuda.cpp
@@ -1446,7 +1446,7 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
                     size = line + " ";
             }
             
-            std::string file_path = "./" + buff_name + ".txt";
+            std::string file_path = "\"./" + buff_name + ".txt\"";
             if ( buff->get_location() != memory_location::global ) continue;
             
             ss << "std::cout << \"Used GPU buffer :" << buff_name << "\" << std::endl; " << "\n";
