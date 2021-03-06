c754712f8e14547ebc1efc1ca0afcf2476c221bf
NEDJIMAbelgacem <gb_nedjima@esi,dz>
2021-03-06 16:16:08 +0100

    use get_line not std::get_line

--- src/tiramisu_codegen_cuda.cpp
+++ src/tiramisu_codegen_cuda.cpp
@@ -938,7 +938,7 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
             std::string line;
             if (in.is_open())
             {
-                while ( std::get_line( in, line ) )
+                while ( get_line( in, line ) )
                 {
                     functions = line + "\n";
                 }
@@ -1441,7 +1441,7 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
                 std::stringstream ss;
                 buff->print_size(ss, base, " * ");
                 std::string line;
-                while ( std::get_line( ss, line ) )
+                while ( get_line( ss, line ) )
                     size = line + " ";
             }
             
