44eda52753077dade93aed62ab4af2f2ba100b88
NEDJIMAbelgacem <gb_nedjima@esi,dz>
2021-03-06 16:33:09 +0100

    try to fix reading functions file

--- src/functions.txt.cpp
+++ src/functions.txt.cpp
@@ -7,8 +7,13 @@ void print_global_buffer_to_file( T *device_data, int size, const std::string &f
     cudaError_t cudaStatus = cudaMemcpy(device_data, host_data, size * sizeof( T ), cudaMemcpyHostToDevice);
     std::ofstream output_file;
     output_file.open( file_path.c_str() );
-    for (int i = 0; i < size; ++i)
-        output_file << host_data[i] << "\n";
-    output_file.close();
+    if ( output_file.is_open() )
+    {
+      for (int i = 0; i < size; ++i)
+          output_file << host_data[i] << "\n";
+      output_file.close();
+    } else {
+      std::cout << "Couldn't open file: " << file_path << std::endl;
+    }
     free( host_data );
 }
