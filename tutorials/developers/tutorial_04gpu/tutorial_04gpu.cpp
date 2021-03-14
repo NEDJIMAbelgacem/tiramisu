44fb091b754e5083c55638f4aa04cc3577dd47e6
NEDJIMAbelgacem <gb_nedjima@esi,dz>
2021-03-14 11:05:01 +0100

    change to p_uint32

--- tutorials/developers/tutorial_04gpu/tutorial_04gpu.cpp
+++ tutorials/developers/tutorial_04gpu/tutorial_04gpu.cpp
@@ -28,27 +28,27 @@ int main(int argc, char **argv)
     var i("i", 0, N), j("j", 0, N), k("k", 0, N);
 
     // Declare cpu buffers.
-    buffer b_A("b_A", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_input);
-    buffer b_B("b_B", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_input);
-    buffer b_C("b_C", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_output);
+    buffer b_A("b_A", {expr(SIZE0), expr(SIZE0)}, p_uint32, a_input);
+    buffer b_B("b_B", {expr(SIZE0), expr(SIZE0)}, p_uint32, a_input);
+    buffer b_C("b_C", {expr(SIZE0), expr(SIZE0)}, p_uint32, a_output);
     // Declare gpu buffers.
-    buffer b_A_gpu("b_A_gpu", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_temporary);
-    buffer b_B_gpu("b_B_gpu", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_temporary);
-    buffer b_C_gpu("b_C_gpu", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_temporary);
+    buffer b_A_gpu("b_A_gpu", {expr(SIZE0), expr(SIZE0)}, p_uint32, a_temporary);
+    buffer b_B_gpu("b_B_gpu", {expr(SIZE0), expr(SIZE0)}, p_uint32, a_temporary);
+    buffer b_C_gpu("b_C_gpu", {expr(SIZE0), expr(SIZE0)}, p_uint32, a_temporary);
     // Tag the GPU buffers to be stored in global memory.
     b_A_gpu.tag_gpu_global();
     b_B_gpu.tag_gpu_global();
     b_C_gpu.tag_gpu_global();
 
     // Declare inputs.
-    input A("A", {"i", "j"}, {N, N}, p_uint8);
-    input B("B", {"i", "j"}, {N, N}, p_uint8);
+    input A("A", {"i", "j"}, {N, N}, p_uint32);
+    input B("B", {"i", "j"}, {N, N}, p_uint32);
 
     // Declare a computation to initialize the reduction.
     computation C_init("C_init", {i, j}, expr((uint8_t) 0));
 
     // Declare the reduction operation.
-    computation C("C", {i,j,k}, p_uint8);
+    computation C("C", {i,j,k}, p_uint32);
     // Note that the previous computation has an empty expression,
     // because we can only use C in an expression after its declaration.
     C.set_expression(C(i, j, k - 1) + A(i, k) * B(k, j));
