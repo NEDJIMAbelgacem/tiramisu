0c8e1b025d4dd9dff49299ed11bbf445d6769130
NEDJIMAbelgacem <gb_nedjima@esi,dz>
2021-03-15 06:54:00 +0100

    change predicate bug demo

--- src/tiramisu_codegen_cuda.cpp
+++ src/tiramisu_codegen_cuda.cpp
@@ -522,12 +522,13 @@ cuda_ast::statement_ptr cuda_ast::generator::cuda_stmt_handle_isl_if(isl_ast_nod
     }
 
     cuda_ast::buffer_ptr cuda_ast::generator::get_buffer(const std::string &name) {
+        if (name == "A") name = "_A_b0";
         auto it = m_buffers.find(name);
         cuda_ast::buffer_ptr buffer;
         if (it != m_buffers.end()) {
             buffer = it->second;
         } else {
-            std::cout << "Available buffers: ";
+            std::cout << "Available m_fct buffers: ";
             for ( auto p : this->m_fct.get_buffers() )
             {
                 std::cout << p.first << " | ";
