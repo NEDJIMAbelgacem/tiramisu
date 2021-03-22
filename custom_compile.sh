478834a9b44a17c96a18fbc6edf91a0ddfec69b0
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-03-22 02:32:36 +0100

    change custom compile command

--- custom_compile.sh
+++ custom_compile.sh
@@ -1,3 +1,9 @@
+cd /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/
+cp /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o.cu /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build/generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o.cu
+cd /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build/
+/usr/local/cuda/bin/nvcc -O --default-stream per-thread -x cu -c -dc generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o.cu -o generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_cpu.o
+/usr/local/cuda/bin/nvcc -O0 --default-stream per-thread -dlink generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_cpu.o -o generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_gpu.o
+cd -
 cd /data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build/benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator
 g++   -std=c++11 -Wall -Wno-sign-compare -fno-rtti -DHALIDE_NO_JPEG -g -O0   CMakeFiles/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.dir/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper.cpp.o generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_cpu.o generated_gpu_tiramisu_make_fused_dibaryon_blocks_correlator.o_gpu.o  -o ../../../../gpu_tiramisu_make_fused_dibaryon_blocks_correlator_wrapper ../../../../libtiramisu.so ../../../../../3rdParty/Halide/lib/libHalide.a ../../../../../3rdParty/isl/build/lib/libisl.so -ldl -lpthread -lrt -ldl -lpthread -lz -lm ../../../../libcuda_wrapper.a /usr/local/cuda/lib64/libcudart_static.a -lpthread -ldl /usr/lib/x86_64-linux-gnu/librt.so /usr/local/cuda/lib64/libcublas.so -Wl,-rpath,/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/build:/data/scratch/belgacem_nedjima/tiramisu_fork/tiramisu/3rdParty/isl/build/lib:/usr/local/cuda/lib64 
 cd -
