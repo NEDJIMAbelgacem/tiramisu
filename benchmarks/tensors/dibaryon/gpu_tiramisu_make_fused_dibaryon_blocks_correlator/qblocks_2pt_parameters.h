850fdf861d3db81a04defdf86240b522feda134a
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-04-10 22:24:38 +0100

    change size to 32

--- benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/qblocks_2pt_parameters.h
+++ benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/qblocks_2pt_parameters.h
@@ -7,8 +7,8 @@
 #if DATA_SET_1
 
 
-#define P_Vsrc 4
-#define P_Vsnk 4
+#define P_Vsrc 32
+#define P_Vsnk 32
 #define P_Nsrc 44
 #define P_Nsnk 44
 #define P_NEntangled 3
@@ -31,8 +31,8 @@
 #define P_B0Nrows 1
 #define P_Mw 12
 #define P_NsFull 4
-#define P_sites_per_rank 2 // threads per block
-#define P_src_sites_per_rank 2 // blocks
+#define P_sites_per_rank 16
+#define P_src_sites_per_rank 16
 
 #elif DATA_SET_2
 
