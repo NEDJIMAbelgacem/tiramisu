fa3e71e09bb546243c4d546ad08dfd37ebc7b587
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-05-04 10:14:18 +0100

    try changing generator

--- tests/test_197.cpp
+++ tests/test_197.cpp
@@ -29,15 +29,16 @@ int main(int argc, char **argv)
     computation init_A( "init_A", { t, A_iter1, A_iter2 }, expr( (double) 0 ) );
     init_A.store_in( &b_A_gpu, { t, A_iter1, A_iter2 } );
 
-    computation init_A2( "init_A2", { t, A_iter1, A_iter2 }, expr( (double) 1 ) );
-    init_A2.store_in( &b_A_gpu, { t, A_iter1, A_iter2 } );
+    // computation init_A2( "init_A2", { t, A_iter1, A_iter2 }, expr( (double) 1 ) );
+    // init_A2.store_in( &b_A_gpu, { t, A_iter1, A_iter2 } );
 
     tiramisu::computation * allocate_A = b_A_gpu.allocate_at( init_A, t );
 
     init_A.tag_gpu_level( A_iter1, A_iter2 );
-    init_A2.tag_gpu_level( A_iter1, A_iter2 );
+    // init_A2.tag_gpu_level( A_iter1, A_iter2 );
 
-    allocate_A->then( init_A, t ).then( init_A2, A_iter2 );
+    // allocate_A->then( init_A, t );
+    allocate_A->before( init_A, t );
     // init_A.then( init_A2, A_iter2 );
 
     tiramisu::codegen({ A.get_buffer() },
