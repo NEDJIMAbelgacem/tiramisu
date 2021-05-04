6947d5da7a8a137234567263aaf61987862adbf7
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-05-04 10:23:38 +0100

    fix

--- tests/test_197.cpp
+++ tests/test_197.cpp
@@ -29,7 +29,8 @@ int main(int argc, char **argv)
     computation init_A( "init_A", { t, A_iter1, A_iter2 }, expr( (double) 0 ) );
     init_A.store_in( &b_A_gpu, { t, A_iter1, A_iter2 } );
 
-    computation copy_A_buff( {}, memcpy( *A.get_buffer(), b_A_gpu ) );
+    computation copy_A_host_to_device( {}, memcpy( *A.get_buffer(), b_A_gpu ) );
+    computation copy_A_device_to_host( {}, memcpy( b_A_gpu, *A.get_buffer() ) );
 
     // computation init_A2( "init_A2", { t, A_iter1, A_iter2 }, expr( (double) 1 ) );
     // init_A2.store_in( &b_A_gpu, { t, A_iter1, A_iter2 } );
@@ -39,8 +40,10 @@ int main(int argc, char **argv)
     init_A.tag_gpu_level( A_iter1, A_iter2 );
     // init_A2.tag_gpu_level( A_iter1, A_iter2 );
 
-    copy_A_buff.then( *allocate_A, computation::root ).then( init_A, t );
+    copy_A_buff.then( *allocate_A, computation::root ).then( init_A, t ).then( copy_A_device_to_host, computation::root );
     // init_A.then( init_A2, A_iter2 );
+
+
 
     tiramisu::codegen({ A.get_buffer() },
                       "build/generated_fct_test_197.o", true);
