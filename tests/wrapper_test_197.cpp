7680f44251551935b6dffbb313c18bf5cddded47
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-05-07 16:03:37 +0100

    change

--- tests/wrapper_test_197.cpp
+++ tests/wrapper_test_197.cpp
@@ -5,10 +5,10 @@
 
 void test_allocation(const std::string &name)
 {
-    Halide::Buffer<int32_t> A(  A_size, A_size, T_size, "buf_A" );
-    Halide::Buffer<int32_t> A_ref( A_size, A_size, T_size, "buf_A_ref" );
-    Halide::Buffer<int32_t> B( B_size, B_size, T_size, "buf_B" );
-    Halide::Buffer<int32_t> B_ref( B_size, B_size, T_size, "buf_B_ref" );
+    Halide::Buffer<int32_t> A(  A_size, A_size, T_size );
+    Halide::Buffer<int32_t> A_ref( A_size, A_size, T_size );
+    Halide::Buffer<int32_t> B( B_size, B_size, T_size );
+    Halide::Buffer<int32_t> B_ref( B_size, B_size, T_size );
     for (int i = 0; i < A_size; ++i)
     {
         for (int j = 0; j < A_size; ++j)
@@ -35,52 +35,10 @@ void test_allocation(const std::string &name)
         }
     }
 
-    std::cout << A(0, 0, 0) << " | " << A_ref(0, 0, 0) << "\n";
-
     test_197( A.raw_buffer(), B.raw_buffer() );
 
-    for (int i = 0; i < B_size; ++i)
-    {
-        for (int j = 0; j < B_size; ++j)
-        {
-            for (int t = 0; t < T_size; ++t)
-            {
-                if ( B( i, j, t ) > 0 )
-                {
-                    std::cout << i << " " << j << " " << t << " | " << B( i, j, t ) << "\n";
-                }
-            }
-        }
-    }
-
-    float sum_A = 0;
-    for (int i = 0; i < A_size; ++i)
-    {
-        for (int j = 0; j < A_size; ++j)
-        {
-            for (int t = 0; t < T_size; ++t)
-            {
-                sum_A += A(i, j, t);
-            }
-        }
-    }
-    float sum_A_ref = 0;
-    for (int i = 0; i < A_size; ++i)
-    {
-        for (int j = 0; j < A_size; ++j)
-        {
-            for (int t = 0; t < T_size; ++t)
-            {
-                sum_A_ref += A(i, j, t);
-            }
-        }
-    }
-    
-    std::cout << "sum_A: " << sum_A << "\n";
-    std::cout << "sum_A_ref: " << sum_A_ref << "\n";
-
-    compare_buffers(name + "_B_check", B, B_ref);
     compare_buffers(name + "_A_check", A, A_ref);
+    compare_buffers(name + "_B_check", B, B_ref);
 }
 
 int main(int, char **)
