5d350ae78660fc410f95e6c3372088e8e3e37dab
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-05-07 15:18:51 +0100

    change

--- tests/test_197.cpp
+++ tests/test_197.cpp
@@ -10,67 +10,60 @@ int main(int argc, char **argv)
 
     // generated code:
     // {
-    //     for (int t1 = 0; t1 < T; ++t1)
+    //     for (int t = 0; t < T; ++t)
     //     {
-    //         for (int t2 = 0; t2 < T; ++t2)
-    //         {
-    //             allocate A_gpu
-    //             initialize A_gpu to 1
-    //             copy from A_gpu (device) to A (host)
-    //             deallocate A_gpu
-    //         }
+    //         allocate A_gpu
+    //         initialize A_gpu to 1
+    //         copy from A_gpu (device) to A (host)
+    //         deallocate A_gpu
     //     }
-    //     for (int t1 = 0; t1 < T; ++t1)
+    //     for (int t = 0; t < T; ++t)
     //     {
-    //         for (int t2 = 0; t2 < T; ++t2)
-    //         {
-    //             allocate B_gpu
-    //             initialize B_gpu to 2
-    //             copy from B_gpu (device) to B (host)
-    //             deallocate B_gpu
-    //         }
+    //         allocate B_gpu
+    //         initialize B_gpu to 2
+    //         copy from B_gpu (device) to B (host)
+    //         deallocate B_gpu
     //     }
     // }
-    var t1( "t1", 0, T_size );
-    var t2( "t2", 0, T_size );
+    var t( "t", 0, T_size );
     var A_iter1( "A_iter1", 0, A_size );
     var A_iter2( "A_iter2", 0, A_size );
 
     var B_iter1( "B_iter1", 0, B_size );
     var B_iter2( "B_iter2", 0, B_size );
 
-    input A( "A", { t1, t2, A_iter1, A_iter2 }, p_float32 );
-    input B( "B", { t1, t2, B_iter1, B_iter2 }, p_float32 );
+    input A( "A", { t, A_iter1, A_iter2 }, p_float32 );
+    input B( "B", { t, B_iter1, B_iter2 }, p_float32 );
 
-    buffer b_A_gpu( "b_A_gpu", { T_size, T_size, A_size, A_size }, p_float32, a_temporary );
+    buffer b_A_gpu( "b_A_gpu", { T_size, A_size, A_size }, p_float32, a_temporary );
     b_A_gpu.tag_gpu_global();
 
-    buffer b_B_gpu( "b_B_gpu", { T_size, T_size, B_size, B_size }, p_float32, a_temporary );
+    buffer b_B_gpu( "b_B_gpu", { T_size, B_size, B_size }, p_float32, a_temporary );
     b_B_gpu.tag_gpu_global();
 
-    computation init_A( "init_A", { t1, t2, A_iter1, A_iter2 }, expr( (float) -1 ) );
-    init_A.store_in( &b_A_gpu, { t1, t2, A_iter1, A_iter2 } );
+    computation init_A( "init_A", { t, A_iter1, A_iter2 }, expr( (float) -1 ) );
+    init_A.store_in( &b_A_gpu, { t, A_iter1, A_iter2 } );
     init_A.tag_gpu_level( A_iter1, A_iter2 );
 
-    computation init_B( "init_B", { t1, t2, B_iter1, B_iter2 }, expr( (float) 2 ) );
-    init_B.store_in( &b_B_gpu, { t1, t2, B_iter1, B_iter2 } );
+    computation init_B( "init_B", { t, B_iter1, B_iter2 }, expr( (float) 2 ) );
+    init_B.store_in( &b_B_gpu, { t, B_iter1, B_iter2 } );
     init_B.tag_gpu_level( B_iter1, B_iter2 );
 
-    computation copy_A_device_to_host( {t1, t2}, memcpy( b_A_gpu, *A.get_buffer() ) );
-    computation copy_B_device_to_host( {t1, t2}, memcpy( b_B_gpu, *B.get_buffer() ) );
+    computation copy_A_device_to_host( {t}, memcpy( b_A_gpu, *A.get_buffer() ) );
+    computation copy_B_device_to_host( {t}, memcpy( b_B_gpu, *B.get_buffer() ) );
 
-    tiramisu::computation *allocate_A = b_A_gpu.allocate_at( init_A, t2 );
+    tiramisu::computation *allocate_A = b_A_gpu.allocate_at( init_A, t );
     tiramisu::computation *deallocate_A = b_A_gpu.deallocate_at( copy_A_device_to_host, t2 );
 
-    tiramisu::computation *allocate_B = b_B_gpu.allocate_at( init_B, t2 );
+    tiramisu::computation *allocate_B = b_B_gpu.allocate_at( init_B, t );
     tiramisu::computation *deallocate_B = b_B_gpu.deallocate_at( copy_B_device_to_host, t2 );
 
-    allocate_A->then( init_A, t2 ).then( copy_A_device_to_host, t2 )
-                                  .then( *deallocate_A, t2 )
+    allocate_A->then( init_A, t ).then( copy_A_device_to_host, t )
+                                  .then( *deallocate_A, t )
                                   .then( *allocate_B, computation::root )
-                                  .then( init_B, t2 )
-                                  .then( copy_B_device_to_host, t2 )
-                                  .then( *deallocate_B, t2 );
+                                  .then( init_B, t )
+                                  .then( copy_B_device_to_host, t )
+                                  .then( *deallocate_B, t );
 
     tiramisu::codegen({ A.get_buffer(), B.get_buffer() }, "build/generated_fct_test_197.o", true);
     return 0;
--- tests/wrapper_test_197.cpp
+++ tests/wrapper_test_197.cpp
@@ -6,22 +6,19 @@
 
 void test_allocation(const std::string &name)
 {
-    Halide::Buffer<float> A( std::vector<int> { A_size, A_size, T_size, T_size } );
-    Halide::Buffer<float> A_ref( std::vector<int> { A_size, A_size, T_size, T_size } );
-    Halide::Buffer<float> B( std::vector<int> { B_size, B_size, T_size, T_size } );
-    Halide::Buffer<float> B_ref( std::vector<int> { B_size, B_size, T_size, T_size } );
+    Halide::Buffer<float> A( std::vector<int> { A_size, A_size, T_size } );
+    Halide::Buffer<float> A_ref( std::vector<int> { A_size, A_size, T_size } );
+    Halide::Buffer<float> B( std::vector<int> { B_size, B_size, T_size } );
+    Halide::Buffer<float> B_ref( std::vector<int> { B_size, B_size, T_size } );
     for (int i = 0; i < A_size; ++i)
     {
         for (int j = 0; j < A_size; ++j)
         {
-            for (int t2 = 0; t2 < T_size; ++t2)
+            for (int t = 0; t < T_size; ++t)
             {
-                for (int t1 = 0; t1 < T_size; ++t1)
-                {
-                    int val = std::rand() % 10 - 5;
-                    A(i, j, t2 * T_size + t1) = val;
-                    A_ref(i, j, t2 * T_size + t1) = 1;
-                }
+                int val = std::rand() % 10 - 5;
+                A(i, j, t) = val;
+                A_ref(i, j, t) = 1;
             }
         }
     }
@@ -30,14 +27,11 @@ void test_allocation(const std::string &name)
     {
         for (int j = 0; j < B_size; ++j)
         {
-            for (int t2 = 0; t2 < T_size; ++t2)
+            for (int t = 0; t < T_size; ++t)
             {
-                for (int t1 = 0; t1 < T_size; ++t1)
-                {
-                    int val = std::rand() % 10 - 5;
-                    B(i, j, t2 * T_size + t1) = val;
-                    B_ref(i, j, t2 * T_size + t1) = 2;
-                }
+                int val = std::rand() % 10 - 5;
+                B(i, j, t) = val;
+                B_ref(i, j, t) = 2;
             }
         }
     }
