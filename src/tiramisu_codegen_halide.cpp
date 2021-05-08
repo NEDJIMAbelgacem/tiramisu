b87d05f7ca97dcf5ce7a9036dd7fd652c9134753
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-05-08 11:40:43 +0100

    change

--- src/tiramisu_codegen_halide.cpp
+++ src/tiramisu_codegen_halide.cpp
@@ -1762,13 +1762,8 @@ tiramisu::generator::halide_stmt_from_isl_node(const tiramisu::function &fct, is
                 }
                 else if (op_type == tiramisu::o_free)
                 {
-                    std::string buffer_name = comp->get_expr().get_name();
-                    DEBUG(10, tiramisu::str_dump("The computation of the node is free IR node."));
-                    DEBUG(10, tiramisu::str_dump("The buffer that should be freed is " + buffer_name));
-                    tiramisu::buffer *buf = comp->get_function()->get_buffers().find(buffer_name)->second;
-                    // if ( buf->get_location() != cuda_ast::memory_location::global )
-                        // buf = (comp->get_access_relation() != nullptr) ? fct.get_buffers().at(get_buffer_name(comp)) : nullptr;
-                    block = make_buffer_free( buf );
+                    auto * buffer = (comp->get_access_relation() != nullptr) ? fct.get_buffers().at(get_buffer_name(comp)) : nullptr;
+                    block = generator::make_buffer_free(buffer);
                 }
                 else
                 {
