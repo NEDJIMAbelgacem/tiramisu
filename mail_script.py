97a09b1897878c49f9ec946e213b9468012412a1
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-05-09 14:21:08 +0100

    add sendmail script

---
+++ mail_script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import smtplib
+
+sender = 'dgx1@mail.com'
+receivers = ['gb_nedjima@esi.dz']
+
+message = """From: From Person <dgx1@mail.com>
+To: To Person <gb_nedjima@esi.dz>
+Subject: Your script has finished
+"""
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)         
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
