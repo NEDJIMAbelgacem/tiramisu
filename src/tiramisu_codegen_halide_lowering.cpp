8ac2f3231688b3ce99368d1ce5da4363e1601012
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-06-07 00:20:45 +0100

    disable one simplification lowering

--- src/tiramisu_codegen_halide_lowering.cpp
+++ src/tiramisu_codegen_halide_lowering.cpp
@@ -117,11 +117,11 @@ Module lower_halide_pipeline(const string &pipeline_name,
                   stmt_to_string("Lowering after injecting per-block gpu synchronization:\n", s)));
     }
 
-    DEBUG(3, tiramisu::str_dump("Simplifying...\n"));
-    s = simplify(s);
-    s = unify_duplicate_lets(s);
-    s = remove_trivial_for_loops(s);
-    DEBUG(4, tiramisu::str_dump(stmt_to_string("Lowering after second simplifcation:\n", s)));
+    // DEBUG(3, tiramisu::str_dump("Simplifying...\n"));
+    // s = simplify(s);
+    // s = unify_duplicate_lets(s);
+    // s = remove_trivial_for_loops(s);
+    // DEBUG(4, tiramisu::str_dump(stmt_to_string("Lowering after second simplifcation:\n", s)));
 
     if (PRINT_HALIDE_IR_AFTER_CODEGEN)
     {
