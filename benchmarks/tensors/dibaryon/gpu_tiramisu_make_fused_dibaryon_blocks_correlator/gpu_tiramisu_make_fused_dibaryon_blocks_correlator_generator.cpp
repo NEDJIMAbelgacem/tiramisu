27b1b8cd43723c1571a372656904d96169bd0613
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-06-17 14:34:01 +0100

    fix schedule

--- benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_generator.cpp
+++ benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/gpu_tiramisu_make_fused_dibaryon_blocks_correlator_generator.cpp
@@ -3323,7 +3323,7 @@ void generate_function(std::string name)
           .then( *allocate_buf_B1_Bthird_props_r1_r, t)
           .then( *allocate_buf_B1_Bthird_props_r1_i, t)
 // 
-          .then(B1_Blocal_r1_r_props_init, r ) // t, x1, x2, iCprime, iSprime, kCprime, kSprime, y, jCprime, jSprime
+          .then(B1_Blocal_r1_r_props_init, t ) // t, x1, x2, iCprime, iSprime, kCprime, kSprime, y, jCprime, jSprime
           .then(B1_Blocal_r1_i_props_init, jSprime)
           .then(B1_Bfirst_r1_r_props_init, jSprime)
           .then(B1_Bfirst_r1_i_props_init, jSprime)
